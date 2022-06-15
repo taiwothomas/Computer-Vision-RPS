e6f0994a0c0339964084dcc992039342277d96ce
Taiwo Thomas <twthms23@gmail.com>
2022-06-15 20:38:42 +0100

    Added functions to determine the winner and play the game.

--- manual_rps.py
+++ manual_rps.py
@@ -1,3 +1,4 @@
+from curses import KEY_MARK
 import random
 
 choice = ["Rock", "Paper", "Scissors"]
@@ -11,3 +12,19 @@ def get_user_choice():
     user_choice = choice[num]
     return user_choice
 
+def get_winner(player_1, player_2):
+    global choice
+    first = choice.index(player_1)
+    second = choice.index(player_2)
+    result = first - second
+    if result == 0:
+        print("It's a draw")
+    elif result == -1 or 2:
+        print("You lose.")
+    else:
+        print("You win!")
+
+def play():
+    get_winner(get_user_choice(), get_computer_choice())
+
+play()
