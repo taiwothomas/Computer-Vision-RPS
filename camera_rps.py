58b042075c3840977c7c8544374e85d45cae7a51
Taiwo Thomas <twthms23@gmail.com>
2022-06-16 17:30:56 +0100

    Completed the get_prediction funtion

--- camera_rps.py
+++ camera_rps.py
@@ -4,7 +4,10 @@ def get_prediction():
     for n in range(1,3):
         if prediction[n]>prediction[n+1]:
             user_choice = prediction[n]
+        else:
+            user_choice = prediction[n+1]
+    return user_choice
 
 # the cv model prints prediction for the last images before you press 'q'
-# it's a list of probabily values for each of the four options
+# it's a list of probability values for each of the four options
 # take the highest prediction and use it to enter the user_choice
