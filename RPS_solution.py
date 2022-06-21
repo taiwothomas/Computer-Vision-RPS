60a8fc172564816b94c4028931cba416db321601
Taiwo Thomas <twthms23@gmail.com>
2022-06-21 20:57:11 +0100

    added a timer to the while loop in RPS_soltion.py

--- RPS_solution.py
+++ RPS_solution.py
@@ -1,11 +1,13 @@
+import time
 import cv2
 from keras.models import load_model
 import numpy as np
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+t = time.time()
 
-while True: 
+while t > time.time()-5: 
     ret, frame = cap.read()
     resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
     image_np = np.array(resized_frame)
@@ -15,6 +17,7 @@ while True:
     cv2.imshow('frame', frame)
     # Press q to close the window
     print(prediction)
+    # t = time.time() - 1
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
             
