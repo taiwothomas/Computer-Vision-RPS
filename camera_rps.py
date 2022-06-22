0e995e1bd927b48d15117e587468565b77fdfcea
Taiwo Thomas <twthms23@gmail.com>
2022-06-22 21:06:22 +0100

    Added a countdown for the computer vision frame and gave the game five rounds with the final scores compared and the winner given.

--- camera_rps.py
+++ camera_rps.py
@@ -1,9 +1,12 @@
+from pkg_resources import run_script
 import numpy
-from RPS_solution import prediction
+from RPS_solution import vision
 
 def get_prediction():
-    number = numpy.argmax(prediction)
+    number = numpy.argmax(vision()) - 1
     return number
+
+
 
 # the cv model prints prediction for the last images before you press 'q'
 # it's a list of probability values for each of the four options
--- manual_rps.py
+++ manual_rps.py
@@ -4,6 +4,9 @@ from camera_rps import get_prediction
 
 
 choice = ["Rock", "Paper", "Scissors"]
+computer_wins = 0
+user_wins = 0
+rounds_played = 0
 
 def get_computer_choice():
     global choice
@@ -17,7 +20,7 @@ def get_user_choice():
     return user_choice
 
 def get_winner(player_1, player_2):
-    global choice
+    global choice, computer_wins, user_wins, rounds_played
     first = choice.index(player_1)
     second = choice.index(player_2)
     result = first - second
@@ -25,10 +28,22 @@ def get_winner(player_1, player_2):
         print("It's a draw")
     elif result == -1 or 2:
         print("You lose.")
+        computer_wins += 1
     else:
         print("You win!")
+        user_wins += 1
+    rounds_played += 1
+    return computer_wins, user_wins
 
 def play():
     get_winner(get_user_choice(), get_computer_choice())
 
-play()
+while rounds_played < 5:
+    play()
+    
+if computer_wins > user_wins:
+    print("You have lost the war.")
+elif user_wins > computer_wins:
+    print("You have won the war.")
+else:
+    print("Stalemate. Impasse. Deadlock. Unanimous draw.")
